2a5d571244a1c36f5214c2fc7a6b371b45441a7e
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-11-17 17:07:41 -0600

    factory y seeders

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Schema\Blueprint;
 
 class Venta extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
 }
--- database/seeders/VentasSeeder.php
+++ database/seeders/VentasSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Venta;
 use Illuminate\Database\Seeder;
 
 class VentasSeeder extends Seeder
@@ -13,6 +14,6 @@ class VentasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Venta::factory()->count(10)->create();
     }
 }
